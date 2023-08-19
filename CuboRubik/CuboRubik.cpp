c6b06f270ec68917b3ae75dbbdd2223784724de4
Abdías Ureña <abdias.urena.soto@est.una.ac.cr>
2023-08-18 20:48:58 -0600

    fix files

--- CuboRubik/CuboRubik.cpp
+++ CuboRubik/CuboRubik.cpp
@@ -13,9 +13,9 @@ int main()
 	win1.create(sf::VideoMode(1300, 700), "Play", sf::Style::Titlebar | sf::Style::Close);
 	win1.setPosition(centerWindow);
 
-	sf::RenderWindow win2;
+	/*sf::RenderWindow win2;
 	win2.create(sf::VideoMode(1300, 700), "Second Win", sf::Style::Titlebar | sf::Style::Close);
-	win2.setPosition(centerWindow);
+	win2.setPosition(centerWindow);*/
 
 	Button buttonPlay;
 	buttonPlay.setShapePosition(559, 320);
@@ -31,23 +31,8 @@ int main()
 		{
 			if (e.type == sf::Event::Closed)
 				win1.close();
-			if (e.type == sf::Event::MouseButtonPressed && buttonPlay.isPressed(sf::Vector2f::Vector2(sf::Mouse::getPosition(win1).x,
-				sf::Mouse::getPosition(win1).y), 0)) {
-				win1.~RenderWindow();
-				win2.clear();
-				win2.display();
-			}
-		}
-		sf::Event e1;
-		while (win2.pollEvent(e))
-		{
-			if (e1.type == sf::Event::Closed)
-			{
-				win2.close();
-			}
 		}
 		win1.clear();
-		win1.~RenderWindow();
 		buttonPlay.drawMe(win1);
 		win1.display();
 	}
